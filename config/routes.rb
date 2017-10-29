3f0e57977e5c68eb8a9f9df4202b6b37e5240ca7
Nirav <nirav@email.com>
2017-10-29 22:15:07 +0000

    Created a new rails app Challeng4.5 and added two action home and about those two action is updated with the heading, paragraph. Changes the root towarsd the actions and edit the links.

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'pages/home', to: 'pages#home', as:'home'
+
+  get 'pages/about_us', to:'pages#about', as:'about'
+  
+  root to: 'pages#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
