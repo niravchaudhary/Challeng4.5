4d662936f3dc4028c920ec41e9b0165e51e502a8
Nirav <nirav@email.com>
2017-10-30 01:26:23 +0000

    Modified the the seed by adding 100 students

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Student.create(firstname:Faker::Name.first_name,
+                 lastname:Faker::Name.last_name)
+  end
